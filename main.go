cc14459da956786fb90794f06a3bbb93c6bd9fb4
Kleine <renatasgal420@gmail.com>
2023-08-23 16:05:44 +0300

    interfaces, methods

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type User struct {
+	Name string `example:"name"`
+}
+
+func (u *User) String() string {
+	return fmt.Sprintf("Hi! My name is %s", u.Name)
+}
+
+func main() {
+	u := &User{
+		Name: "Sammy",
+	}
+
+	fmt.Println(u)
+}
+
